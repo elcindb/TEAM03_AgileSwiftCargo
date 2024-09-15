29e1c2c1647114ab3aef7cb49772eead32b095ab
Ali Usta <aliustafr@gmail.comm>
2024-09-15 23:39:22 +0200

    150924

--- src/test/java/pages/Admin.java
+++ src/test/java/pages/Admin.java
@@ -1,4 +1,44 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class Admin {
+
+    public Admin(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//a[text()='Branch Manage']")
+    public WebElement branchManageDashboard;
+
+    @FindBy(xpath = "(//a[text()='Branch'])[2]")
+    public WebElement branch;
+
+    @FindBy(xpath = "(//a[@href='https://qa.agileswiftcargo.com/admin/hubs'])[3]")
+    public WebElement clearBranch;
+
+    @FindBy(xpath = "//button[@class='btn btn-space btn-primary']")
+    public WebElement filterBranch;
+
+    @FindBy(xpath = "(//button[@tabindex='-1'])[1]")
+    public WebElement actionsBranch;
+
+    @FindBy(xpath = "//a[@href='https://qa.agileswiftcargo.com/admin/hubs/create']")
+    public WebElement plusBranch;
+
+    @FindBy(xpath = "(//input[@type='text'])[2]")
+    public WebElement hubNameTextBranch;
+
+    @FindBy(xpath = "(//input[@type='text'])[3]")
+    public WebElement phoneTextBranch;
+
+
+
+
+
+
 }
--- src/test/java/stepdefinitions/US18_StepDefinitions.java
+++ src/test/java/stepdefinitions/US18_StepDefinitions.java
@@ -1,11 +1,15 @@
 package stepdefinitions;
 
+import io.cucumber.java.bs.A;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import pages.Merchant;
 import utilities.ConfigReader;
 import utilities.Driver;
@@ -15,6 +19,10 @@ import java.util.List;
 public class US18_StepDefinitions {
 
     Merchant agileSwift= new Merchant();
+
+    JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+
+    Actions actions =new Actions(Driver.getDriver());
 
     @Given("The user is logged in to the Merchant page")
     public void the_user_is_logged_in_to_the_merchant_page() {
@@ -34,7 +42,9 @@ public class US18_StepDefinitions {
     @When("The user clicks on the profile icon in the top bar")
     public void the_user_clicks_on_the_profile_icon_in_the_top_bar() {
 
-        agileSwift.profileIconInTheTopBar.click();
+        actions.moveToElement(agileSwift.profileIconInTheTopBar).click().perform();
+
+        //js.executeScript("arguments[0].click();", agileSwift.profileIconInTheTopBar);
 
     }
 
@@ -102,8 +112,7 @@ public class US18_StepDefinitions {
             Driver.getDriver().findElement(By.xpath("(//a[text()='"+"\n" +
                     "                                                "+string+"\n" +
                     "                                            "+"'])[1]")).click();
-        }
-        else Driver.getDriver().findElement(By.xpath("(//a[text()='"+string+"'])[1]")).click();
+        } else Driver.getDriver().findElement(By.xpath("(//a[text()='"+string+"'])[1]")).click();
 
 
     }
@@ -127,9 +136,50 @@ public class US18_StepDefinitions {
         Assertions.assertEquals(expectedFields.get(7),agileSwift.tradeLicenceProfile.getText());
 
 
+    }
 
+
+
+    @When("Click the Edit button on the Profile page")
+    public void click_the_edit_button_on_the_profile_page() {
+
+        agileSwift.editButtonMerchantProfile.click();
 
     }
+    @When("the user modifies the Phone to {string}")
+    public void the_user_modifies_the_phone_to(String string) {
+
+        if (string.equals("newPhone")){
+            string=ConfigReader.getProperty("merchantPhoneNew");
+        }else {
+            string=ConfigReader.getProperty("merchantPhone");
+        }
+        agileSwift.mobileTexteBoxMerchantProfile.clear();
+
+        agileSwift.mobileTexteBoxMerchantProfile.sendKeys(string);
+    }
+    @When("the user clicks the Save button")
+    public void the_user_clicks_the_save_button() {
+
+        js.executeScript("arguments[0].click();", agileSwift.saveChangeButtonProfile);
+
+    }
+    @Then("the profile should be updated with the new information")
+    public void the_profile_should_be_updated_with_the_new_information() {
+
+        if(agileSwift.valeurPhoneProfile.getText().equals(ConfigReader.getProperty("merchantPhoneNew"))){
+            Assertions.assertEquals(agileSwift.valeurPhoneProfile.getText(),ConfigReader.getProperty("merchantPhoneNew"));
+        }
+        if (agileSwift.valeurPhoneProfile.getText().equals(ConfigReader.getProperty("merchantPhone"))){
+            Assertions.assertEquals(agileSwift.valeurPhoneProfile.getText(),ConfigReader.getProperty("merchantPhone"));
+        }
+
+    }
+
+
+
+
+
 
 
 
